084d16dfb73a72fd3004f107ca696a1cca5b58d1
Connor Sedman <c.sedman@vatpac.org>
2021-01-25 16:31:24 +0000

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/collage', function () {
+    return view('Collage');
+});
+Route::get('/collage/corses', function () {
+    return view('corses');
+});
